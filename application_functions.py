6b181192ff18084ce7383beb52a9fe642115a897
KennethHefford <kennethhefford@outlook.com>
2024-05-14 19:53:34 +1000

    Removed new line issue when writing to csv file

--- application_functions.py
+++ application_functions.py
@@ -4,7 +4,7 @@ import csv
 def add_good(file_name):
     print("Add Good Habit")
     good_name = input("Enter a Good Habit: ")
-    with open(file_name, "a") as f:
+    with open(file_name, "a", newline='') as f:
         writer = csv.writer(f)
         writer.writerow([good_name, 1])
 
@@ -13,7 +13,7 @@ def add_good(file_name):
 def add_bad(file_name):
     print("Add Bad Habit")
     bad_name = input("Enter a Bad Habit: ")
-    with open(file_name, "a") as f:
+    with open(file_name, "a", newline='') as f:
         writer = csv.writer(f)
         writer.writerow([bad_name, -1])
 
@@ -22,7 +22,7 @@ def check_points():
 
 def habits_list(file_name):
     print("Check habits tracked")
-    with open(file_name, "r") as f:
+    with open(file_name, "r", newline='') as f:
         reader = csv.reader(f)
         reader.__next__()
         for row in reader:
